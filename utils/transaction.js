e64a22061873114b25d4f2e33314f86cc8fd44be
Abel Abraham Camarillo Ojeda <acamari@verlet.org>
2021-06-25 23:03:54 -0500

    Revert "adds transaction helper"
    This reverts commit df28fcb81cb3267bf19e6bdf7842869f1bd14c1a.

--- utils/transaction.js
+++
@@ -1,7 +0,0 @@
-// helper to create a transaction
-const db = require('../models');
-// XXX for unknown reasons the following fails:
-// 	const { transaction } = db.sequelize;
-// 	transaction(...);
-// so instead do a full wrapper
-module.exports = (...args) => console.log(db) || db.sequelize.transaction(...args);
