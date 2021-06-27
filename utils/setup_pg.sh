de57880cb863acdbf9b1a13fa8ea12a8aca154c2
Abel Abraham Camarillo Ojeda <acamari@verlet.org>
2021-06-26 20:21:36 -0500

    clarify docs

--- utils/setup_pg.sh
+++ utils/setup_pg.sh
@@ -10,10 +10,16 @@ adminuser=powerviews_admin
 createuser -U $SUPERUSER -r $adminuser
 createdb -U $SUPERUSER -O $adminuser $db
 psql -f/dev/stdin -U $SUPERUSER $db <<SQL
+	-- prevent users from creating public objects
 	REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+	-- create exclusive schema for adminuser
 	CREATE SCHEMA IF NOT EXISTS $adminuser AUTHORIZATION $adminuser;
+	-- prevent adminuser for creating public objects
 	ALTER ROLE $adminuser IN DATABASE $db SET search_path = '\$user';
-	-- used only to group users for easy deletion
+	-- we don't use privilege inheritance, but we require a method to group
+	-- powerviews postgresql users
 	CREATE ROLE powerviews_users NOINHERIT;
 SQL
+
+# populate database schema
 node utils/sync_db.js
