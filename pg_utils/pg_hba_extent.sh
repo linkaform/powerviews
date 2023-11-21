985df7e990ca63ec5ca5747d63c274b7e500b236
Abel Abraham Camarillo Ojeda <acamari@verlet.org>
2023-11-20 22:03:59 -0600

    fix docker subnet for pg_hba

--- pg_utils/pg_hba_extent.sh
+++ pg_utils/pg_hba_extent.sh
@@ -5,4 +5,4 @@
 # that user as owner
 
 echo "host    powerviews     +powerviews_users        0.0.0.0/0       md5" >> /var/lib/postgresql/data/pg_hba.conf
-echo "host    powerviews      powerviews_admin        172.16.0.0/16   md5" >> /var/lib/postgresql/data/pg_hba.conf
+echo "host    powerviews      powerviews_admin        172.13.0.0/16   md5" >> /var/lib/postgresql/data/pg_hba.conf
